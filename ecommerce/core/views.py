c4f0fcf6f65e093f1eb0ec2b1c15d255e56de51c
berrondo <claudio.berrondo@gmail.com>
2020-09-14 02:48:29 +0000

    login logout user, products list, orders

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 def home(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    
     products = Product.objects.all()
-    return render(request, 'core/home.html', {'products': products})
+    orders = list(Order.objects.filter(customer__username=username).all())
+    return render(request, 'core/home.html', {'products': products, 'orders': orders})

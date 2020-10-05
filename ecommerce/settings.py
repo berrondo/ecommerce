8d4d73f23ae8a3b07caf9b061061020d89a48902
berrondo <claudio.berrondo@gmail.com>
2020-10-05 04:57:47 +0000

    trying postgres settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -81,20 +81,20 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-# default_dburl = f'sqlite:///{BASE_DIR}/db.sqlite3'
-# DATABASES = {
-#     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-# }
+default_dburl = f'sqlite:///{BASE_DIR}/db.sqlite3'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
